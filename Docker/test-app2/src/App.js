b873f0866cd0308aa7e5a85a6d6b687e7ec395d7
Jaysi <jaysi@jaysi.jaysi>
2024-02-17 21:30:09 +0530

    Learned commands of docker

---
+++ Docker/test-app2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello From Docker</p>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
